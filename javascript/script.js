39bea128a423bc32854ccda2fa61b22442c9e62d
[Binetuu] <binetouseye@weccoo.com>
2026-04-11 16:33:04 +0000

    ajout de la fonction de chargement du projet

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,18 @@
 let projets = [];
 // URL de l'API json-server
 const API_URL = "http://localhost:3000/projets";
+
+// ============================================================
+// CHARGER LES PROJETS — GET
+// ============================================================
+async function chargerProjets() {
+  try {
+    const response = await fetch(API_URL);
+    projets = await response.json();
+    projets.forEach(p => {
+      creerProjet(p.id, p.libelle, p.image);
+    });
+  } catch (error) {
+    console.error("Erreur chargement projets :", error);
+  }
+}
